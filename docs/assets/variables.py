c6211c0a7348055385eff338b255fce884ff419b
Manfred Rosenboom <maroph@pm.me>
2026-09-25 11:15:23 +0200

    add RSS feed to blog

--- docs/assets/variables.py
+++ docs/assets/variables.py
@@ -3,7 +3,7 @@ def define_env(env):
     # Variables are accessible as {{ variable }} in templates
     env.variables["author"] = "Manfred Rosenboom"
     env.variables["version"] = "1.0"
-    env.variables["zensical_version"] = "0.0.64"
+    env.variables["zensical_version"] = "0.0.65"
 
     # # Macros are called as {{ greet("World") }}
     # @env.macro
